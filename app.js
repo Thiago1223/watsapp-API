e0d6d72b6268a9d4e364d46c7cc5a33d3a16550a
Thiago1223 <tifreitas10@gmail.com>
2023-03-17 22:28:15 -0300

    endpoint e função dos contatos dos perfis pronto

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const cors = require('cors')
 
 const bodyParser = require('body-parser')
 
-const contatos = require('./modulo/contatos.js')
+const jsonContatos = require('./modulo/index.js')
 
 const app = express()
 
 app.use((request, response, next) => {
-    
+
     response.header('Access-Control-Allow-Origin', '*')
 
     response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
@@ -25,4 +25,34 @@ app.use((request, response, next) => {
 
     next()
 
+})
+
+app.get('/v1/whatsapp/perfil/:id', cors(), async function (request, response, next) {
+
+    let statusCode
+    let dadosPerfis = {}
+    let idPerfil = request.params.id
+
+    if (idPerfil == '' || idPerfil == undefined || idPerfil.length != 1 || isNaN(idPerfil) || idPerfil < 1 || idPerfil > 4) {
+        statusCode = 400
+        dadosPerfis.message = 'Não foi possível processar pois os dados de entrada que foi enviado não corresponde ao exigido, confira o valor, pois não pode ser vazio.'
+    } else {
+        
+        let contatos = jsonContatos.getPerfil(idPerfil)
+
+        if (contatos) {
+            statusCode = 200
+            dadosPerfis = contatos
+        } else {
+            statusCode = 404
+        }
+    }
+
+    response.status(statusCode)
+    response.json(dadosPerfis)
+
+})
+
+app.listen(8080, function(){
+    console.log('Servidor aguardando requisições na porta 8080.')
 })
--- modulo/index.js
+++ modulo/index.js
@@ -1,1 +1,25 @@
-const contatos = require('./contatos.js')
+const jsonContatos = require('./contatos.js')
+
+const getPerfil = (id) => {
+
+    let jsonPerfil = {}
+    let status = false
+
+    jsonContatos.contatos["whats-users"].forEach(contatosDados => {
+        if (contatosDados.id == id || contatosDados.number == id) {
+            jsonPerfil.contacts = contatosDados.contacts
+            status = true
+        }
+    })
+
+    if (status == true) {
+        return jsonPerfil
+    } else {
+        return status
+    }
+    
+}
+
+module.exports = {
+    getPerfil
+}
